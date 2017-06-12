eb21dcf052a0f8d4c069ecb5b502da16132ad5d6
Henry Xiang <tmyygy1987@163.com>
2017-06-12 20:25:14 +0800

    add hideIntent file
    add hideIntent file . it's different than other one.

---
+++ HideIntent.java
@@ -0,0 +1,15 @@
+package com.ares.henry.showintent;
+
+import android.app.*;
+import android.os.Bundle;
+
+/**
+ * Created by henry on 2017/6/11.
+ */
+public class HideIntent extends android.app.Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hide_intent);
+    }
+}
